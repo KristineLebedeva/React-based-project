7bc6529e7edd2ec017e63fbf359bc5ade978f3a0
Other Kristine <kristina.lebedeva@inbox.lv>
2017-11-10 12:36:43 +0200

    Using React for creating YouTube search application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch  from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+const API_KEY = 'AIzaSyAjxk4L1j30pINecZqTja-2fAnPC6QKPhA';
+
+
+// Create a new component which should produce some html
+
+class App extends Component {
+    
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            videos:[],
+            selectedVideo: null
+        };
+        
+        this.videoSearch('surfboards');
+        
+    }
+    
+    
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+    
+    render(){
+        
+        const videoSearch =_.debounce(term => {this.videoSearch(term)}, 300);
+              
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    videos={this.state.videos}
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                />  
+            </div>
+        );
+    }
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDom.render(<App />, document.querySelector('.container'));
